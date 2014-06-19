e0ba6b25b27ce4726d6484bce0f467d7820ffdf9
tel <tel@tels-MacBook-Pro.local>
2014-06-19 03:00:47 -0400

    added forward flux (fflux) package. absolutely does not actually work at this point. build (+)

---
+++ src/c/lm/fflux/FFluxSupervisor.h
@@ -0,0 +1,139 @@
+/*
+ * University of Illinois Open Source License
+ * Copyright 2012-2014 Roberts Group,
+ * All rights reserved.
+ *
+ * Developed by: Roberts Group
+ * 			     Johns Hopkins University
+ * 			     http://biophysics.jhu.edu/roberts/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the Software), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to
+ * do so, subject to the following conditions:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimers.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimers in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * - Neither the names of the Roberts Group, Johns Hopkins University,
+ * nor the names of its contributors may be used to endorse or
+ * promote products derived from this Software without specific prior written
+ * permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ *
+ * Author(s): Elijah Roberts, Max Klein
+ */
+
+#ifndef REPLICATESUPERVISOR_H_
+#define REPLICATESUPERVISOR_H_
+
+#include <list>
+#include <map>
+#include <deque>
+#include <string>
+#include <vector>
+#include "lm/io/DiffusionModel.pb.h"
+#include "lm/io/ReactionModel.pb.h"
+#include "lm/io/TrajectoryLimits.pb.h"
+#include "lm/io/hdf5/SimulationFile.h"
+#include "lm/main/SimulationSupervisor.h"
+#include "lm/message/FinishedWorkUnit.pb.h"
+#include "lm/message/StartedWorkUnit.pb.h"
+#include "lm/replicates/TrajectoryList.h"
+#include "lm/MPI.h"
+#include "lm/Print.h"
+#include "lm/thread/Worker.h"
+
+namespace lm {
+namespace replicates {
+
+using std::deque;
+using std::list;
+using std::map;
+using std::string;
+using std::vector;
+
+class ReplicateSupervisor : public lm::main::SimulationSupervisor
+{
+public:
+    static bool registered;
+    static bool registerClass();
+    static void* allocateObject();
+
+public:
+    ReplicateSupervisor();
+    //ReplicateSupervisor(int * maxSlotsTable, lm::io::hdf5::Hdf5File * file) throw(PthreadException);
+    virtual ~ReplicateSupervisor();
+
+    //virtual void wake() throw(PthreadException);
+    //virtual void abort() throw(PthreadException);
+    //virtual void checkpoint() throw(PthreadException);
+//    virtual int FindRep(int destProc);
+//    virtual int RunRep(int destProc, int replicate);
+
+    //virtual void distributeWorkUnits();
+    //virtual void distributeWorkUnit(deque<Slot *>::iterator slot_it, map<int, TrajectoryAllocator::Trajectory>::iterator traj_it);
+    //virtual void update(lm::work::Result & result);
+
+//    virtual void MPI_MastBcastOut(void *buf, int count, MPI_Datatype datatype, int tag, MPI_Comm comm);
+//
+//    //receive from all nodes, one by one, including master. nodes should use MPI_Send plus the relevant tag to send
+//    template <typename t>
+//    void MPI_MastBcastIn(t * recvtable, int recvcount, MPI_Datatype recvtype, int recvtag, MPI_Comm comm);
+//
+//    template <typename t, int tag>
+//    void bcastThing(void * staticDataBuffer, t * thing);
+//
+//    template <int tag>
+//    void bcastSizeThenBuffer(void * staticDataBuffer, int msgSize);
+
+    //map<int,int> simulationStatusTable;
+protected:
+    virtual void allResourcesRegistered();
+    virtual void startSimulation();
+    virtual void workUnitStarted(const lm::message::StartedWorkUnit& msg);
+    virtual void workUnitFinished(const lm::message::FinishedWorkUnit& msg);
+    virtual void outputWriterStarted(const lm::message::StartedOutputWriter& msg);
+
+protected:
+    lm::io::TrajectoryLimits limits;
+    TrajectoryList* trajectories;
+    long long workUnitCount;
+    int outputWriterProcess;
+    int outputWriterThread;
+
+    /*
+    void * staticDataBuffer;
+    //variables relating to Worker behavior
+    bool shouldCheckpoint;
+    bool shouldAbort;
+
+    // Create a table for the simulation status.
+    // key is replicate number, val is status: 0=waiting to run, 2=finished, other values=(?)(indicate at least not finished)
+    lm::io::hdf5::Hdf5File * file;
+
+    //the objects that manage the slots and the trajectories
+    TrajectoryAllocator trajectoryAllocator;
+    SupervisorSlotAllocator slotAllocator;
+
+    map<int,struct timespec> simulationStartTimeTable; //TODO: figure out what header timespec is in and put it in this header
+*/
+};
+
+}
+}
+
+#endif /* REPLICATESUPERVISOR_H_ */
---
+++ src/c/lm/fflux/FFluxTrajectoryList.cpp
@@ -0,0 +1,143 @@
+/*
+ * University of Illinois Open Source License
+ * Copyright 2012-2014 Roberts Group,
+ * All rights reserved.
+ *
+ * Developed by: Roberts Group
+ * 			     Johns Hopkins University
+ * 			     http://biophysics.jhu.edu/roberts/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the Software), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to
+ * do so, subject to the following conditions:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimers.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimers in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * - Neither the names of the Roberts Group, Johns Hopkins University,
+ * nor the names of its contributors may be used to endorse or
+ * promote products derived from this Software without specific prior written
+ * permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS WITH THE SOFTWARE.
+ *
+ * Author(s): Elijah Roberts, Max Klein
+ */
+
+#include <list>
+#include <map>
+#include <string>
+
+#include "lm/Print.h"
+#include "lm/io/FirstPassageTimes.pb.h"
+#include "lm/io/ReactionModel.pb.h"
+#include "lm/io/SpeciesCounts.pb.h"
+#include "lm/io/TrajectoryState.pb.h"
+#include "lm/replicates/FFluxTrajectoryList.h"
+
+using std::map;
+using std::string;
+
+namespace lm {
+namespace fflux {
+
+FFluxTrajectoryList::FFluxTrajectoryList(int trajectoryCount, map<string,string>& simulationParameters, const lm::io::ReactionModel& reactionModel)
+{
+    for (int i=0; i<=trajectoryCount; i++)
+    {
+        trajectories[i] = new FFluxTrajectoryStatus(i);
+
+        // Initialize the trajectory id.
+        trajectories[i]->state.set_trajectory_id(i);
+
+        // Initialize the species counts in the cme state.
+        trajectories[i]->state.mutable_cme_state()->mutable_species_counts()->set_trajectory_id(i);
+        trajectories[i]->state.mutable_cme_state()->mutable_species_counts()->set_number_species(reactionModel.number_species());
+        trajectories[i]->state.mutable_cme_state()->mutable_species_counts()->set_number_entries(1);
+        for (int j=0; j<(int)reactionModel.number_species(); j++)
+            trajectories[i]->state.mutable_cme_state()->mutable_species_counts()->add_species_count(reactionModel.initial_species_count(j));
+        trajectories[i]->state.mutable_cme_state()->mutable_species_counts()->add_time(0.0);
+
+        // Initialize the first passage times in the cme state.
+        const string listString = simulationParameters["fptTrackingList"];
+        std::list<int> fptList;
+        size_t start=0, end=0;
+        while (end != string::npos)
+        {
+            end = listString.find(',', start);
+            string trackedSpecies = listString.substr(start, (end == string::npos) ? string::npos : end - start);
+            if (trackedSpecies.length() > 0)
+            {
+                fptList.push_back(atoi(trackedSpecies.c_str()));
+            }
+            start = end+1;
+        }
+        for (std::list<int>::iterator it=fptList.begin(); it != fptList.end(); it++)
+        {
+            lm::io::FirstPassageTimes* fpt= trajectories[i]->state.mutable_cme_state()->add_first_passage_times();
+            fpt->set_trajectory_id(i);
+            fpt->set_species(*it);
+            fpt->set_number_entries(1);
+            fpt->add_species_count(reactionModel.initial_species_count(*it));
+            fpt->add_first_passage_time(0.0);
+            Print::printf(Print::DEBUG, "Added fpt tracking for species %d", *it);
+        }
+    }
+}
+
+FFluxTrajectoryList::~FFluxTrajectoryList()
+{
+    for (map<int,FFluxTrajectoryStatus*>::iterator it=trajectories.begin(); it!=trajectories.end(); it++)
+    {
+        delete it->second;
+    }
+}
+
+int FFluxTrajectoryList::nextTrajectoryToRun()
+{
+    for (map<int,FFluxTrajectoryStatus*>::iterator it=trajectories.begin(); it!=trajectories.end(); it++)
+    {
+        FFluxTrajectoryStatus* t = it->second;
+        if (t->status == NOT_STARTED || t->status == WAITING)
+        {
+            return t->trajectoryNumber;
+        }
+    }
+    return -1;
+}
+
+FFluxTrajectoryList::status_t FFluxTrajectoryList::getTrajectoryStatus(int trajectory)
+{
+    return trajectories[trajectory]->status;
+}
+
+void FFluxTrajectoryList::updateTrajectoryStatus(int trajectory, status_t status)
+{
+    trajectories[trajectory]->status = status;
+}
+
+const lm::io::TrajectoryState& FFluxTrajectoryList::getTrajectoryState(int trajectory)
+{
+    return trajectories[trajectory]->state;
+}
+
+void FFluxTrajectoryList::updateTrajectoryState(int trajectory, const lm::io::TrajectoryState& state)
+{
+    trajectories[trajectory]->state = state;
+}
+
+}
+}
