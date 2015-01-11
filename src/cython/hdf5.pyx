8668a5d19320bae0a02cb6593f29a36046734738
telamonian <mklein@jhu.edu>
2015-01-11 18:19:31 -0500

    first version of cython extension that works with both protobufs and hdf5

--- src/cython/hdf5.pyx
+++ src/cython/hdf5.pyx
@@ -1,16 +1,12 @@
 from libcpp.string cimport string
 from libc.stdint cimport uint32_t  #, int64_t
 
-cdef extern from "lm/io/ReactionModel.pb.h" namespace "lm::io":
-    cdef cppclass ReactionModel:
-        ReactionModel()
-        uint32_t number_species()
-        void set_number_species(uint32_t)
+cimport hdf5_lib
 
-cdef class PyReactionModel:
-    cdef ReactionModel *thisptr
+cdef class ReactionModel:
+    cdef hdf5_lib.ReactionModel* thisptr
     def __cinit__(self):
-        self.thisptr = new ReactionModel()
+        self.thisptr = new hdf5_lib.ReactionModel()
     def __dealloc__(self):
         del self.thisptr
     def number_species(self):
@@ -18,22 +14,18 @@ cdef class PyReactionModel:
     cpdef set_number_species(self, uint32_t numSpec):
         self.thisptr.set_number_species(numSpec)
 
-cdef extern from "lm/io/hdf5/SimulationFile.h" namespace "lm::io::hdf5":
-    cdef cppclass Hdf5File:
-        Hdf5File(string)
-        void close()
-        void getReactionModel(ReactionModel*)
-        void setReactionModel(ReactionModel*)
-
-cdef class PyHdf5File:
-    cdef Hdf5File *thisptr      # hold a C++ instance which we're wrapping
+cdef class Hdf5File:
+    cdef hdf5_lib.Hdf5File* thisptr      # hold a C++ instance which we're wrapping
     def __cinit__(self, string fname):
-        self.thisptr = new Hdf5File(fname)
+        self.thisptr = new hdf5_lib.Hdf5File(fname)
     def __dealloc__(self):
         del self.thisptr
     def close(self):
         self.close()
-    cdef getReactionModel(self, ReactionModel* reactionModel):
-        self.getReactionModel(reactionModel)
-    cdef setReactionModel(self, ReactionModel* reactionModel):
-        self.setReactionModel(reactionModel)
+    def getReactionModel(self, ReactionModel reactionModel):
+        self.thisptr.getReactionModel(reactionModel.thisptr)
+    def setReactionModel(self, ReactionModel reactionModel):
+        self.thisptr.setReactionModel(reactionModel.thisptr)
+        
+# test compiled module in python with this line (assuming that foo.lm is a valid lm input file in the same dir):
+#import hdf5; reactionModel = hdf5.ReactionModel(); hdf5File = hdf5.Hdf5File('foo.lm'); hdf5File.getReactionModel(reactionModel); reactionModel.number_species()
