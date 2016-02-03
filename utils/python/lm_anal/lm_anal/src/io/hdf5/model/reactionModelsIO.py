774bdc91de6f387c6b386e212f44fc078732447c
telamonian <mklein@jhu.edu>
2016-02-03 16:15:03 -0500

    lm_anal: fix for the bugfix

--- utils/python/lm_anal/lm_anal/src/io/hdf5/model/reactionModelsIO.py
+++ utils/python/lm_anal/lm_anal/src/io/hdf5/model/reactionModelsIO.py
@@ -1,6 +1,6 @@
 from lm_anal.src.datum.model.reactionModels import ReactionModels
 from lm_anal.src.spec.io.hdf5 import HDF5IOSpec, HDF5IOSpecs
-from lm_anal.src.io.hdf5.model.modelIO import ModelsIO
+from lm_anal.src.io.hdf5.model.modelsIO import ModelsIO
 
 __all__ = ['ReactionModelsIO']
 
