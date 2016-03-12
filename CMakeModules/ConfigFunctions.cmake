de277a3a1fbe0f9cd10676c4f01d49ee26fa387d
telamonian <max.klein@jhu.edu>
2016-03-12 14:58:16 -0500

    Now, instead of having a semi-ignored CMakeConfig.txt stored at top level in the git repository, the first time cmake is run it will just make a new CMakeConfig.txt for each person who downloads lmes.

--- CMakeModules/ConfigFunctions.cmake
+++ CMakeModules/ConfigFunctions.cmake
@@ -1,5 +1,23 @@
 # a module with functions that make working with the CMakeConfig.txt file easier
 
+# Cmake "cat" function definition. Appends the contents of file at path IN_FILE to file at path OUT_FILE
+function(cat IN_FILE OUT_FILE)
+    file(READ ${IN_FILE} CONTENTS)
+    file(APPEND ${OUT_FILE} "${CONTENTS}")
+    file(APPEND ${OUT_FILE} "\n")
+endfunction()
+
+function(initializeCMakeConfig)
+    set(configTemplatePath ${CMAKE_CURRENT_SOURCE_DIR}/CMakeModules/CMakeConfig.tmpl)
+    set(configPath ${CMAKE_CURRENT_SOURCE_DIR}/CMakeConfig.txt)
+
+    file(GLOB existingCMakeConfig ${configPath})
+    if(NOT existingCMakeConfig)
+        file(WRITE ${configPath} "")
+        cat(${configTemplatePath} ${configPath})
+    endif(NOT existingCMakeConfig)
+endfunction()
+
 # a function that allows for easy set up of default variables while allowing the end-user to override said defaults in CMakeConfig.txt
 # example:
 # SetDefaultWithOverride(CMAKE_C_FLAGS CUSTOM_C_FLAGS
@@ -12,7 +30,7 @@
 # ELSE(DEFINED CUSTOM_C_FLAGS)
 #     SET(CMAKE_C_FLAGS "-g -march=native -fPIC -Wall -fmessage-length=0")
 # ENDIF(DEFINED CUSTOM_C_FLAGS)
-
+#
 function(SetConfigDefault varName configVarName default)
     if(DEFINED ${configVarName})
         set(${varName} ${${configVarName}})
@@ -23,3 +41,4 @@ function(SetConfigDefault varName configVarName default)
     endif(DEFINED ${configVarName})
     set(${varName} ${${varName}} PARENT_SCOPE)
 endfunction()
+
