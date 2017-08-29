50e9c1e519a149de29f35c17b2f7de57fdb990b1
Elijah Roberts <eroberts00@gmail.com>
2017-08-29 16:47:38 -0400

    - Added lm_setdm back to build. - Added second order hill functions.

--- src/c/lm/propensities/GeneticCircuitPropensityFunctions.cpp
+++ src/c/lm/propensities/GeneticCircuitPropensityFunctions.cpp
@@ -670,6 +670,190 @@ public:
     }
 };
 
+class SecondOrderKHillPropensity : public lm::me::PropensityFunction
+{
+public:
+    static const uint REACTION_TYPE = 8014;
+    static const uint REACTION_TYPE_ALTERNATE_FORMAT = 8015;
+
+    SecondOrderKHillPropensity(uint s0i, uint s1i, uint xi, double x0, double k0, double k1, double h) :PropensityFunction(REACTION_TYPE,1),s0i(s1i),s1i(s1i),xi(xi),x0h(pow(x0,h)),k0(k0),dk(k1-k0),h(h) {}
+    int s0i, s1i;
+    uint xi;
+    double x0h;
+    double k0;
+    double dk;
+    double h;
+
+    void changeVolume(double volumeMultiplier) {}
+    double calculate(const double time, const int* speciesCounts, const uint numberSpecies) const
+    {
+        double x = double(speciesCounts[xi]);
+        double xh = pow(x,h);
+        double k = k0+((dk*xh)/(x0h+xh));
+        return k * double(speciesCounts[s0i]) * double(speciesCounts[s1i]);
+    }
+
+#if defined(OPT_AVX) && !defined(OPT_SVML)
+    avxd calculateAvx(const avxd time, const double* speciesCounts, const uint numberSpecies) const
+    {
+        double x0 = speciesCounts[xi*DOUBLES_PER_AVX];
+        double x1 = speciesCounts[xi*DOUBLES_PER_AVX+1];
+        double x2 = speciesCounts[xi*DOUBLES_PER_AVX+2];
+        double x3 = speciesCounts[xi*DOUBLES_PER_AVX+3];
+        avxd xh = _mm256_set_pd(pow(x3,h),pow(x2,h),pow(x1,h),pow(x0,h));
+        avxd k = _mm256_add_pd(_mm256_set1_pd(k0),_mm256_div_pd(_mm256_mul_pd(_mm256_set1_pd(dk),xh),_mm256_add_pd(_mm256_set1_pd(x0h),xh)));
+        return _mm256_mul_pd(k, _mm256_mul_pd(_mm256_load_pd(&speciesCounts[s0i*DOUBLES_PER_AVX]),_mm256_load_pd(&speciesCounts[s1i*DOUBLES_PER_AVX])));
+    }
+#endif
+#if defined(OPT_AVX) && defined(OPT_SVML)
+    avxd calculateAvx(const avxd time, const double* speciesCounts, const uint numberSpecies) const
+    {
+        avxd x = _mm256_load_pd(&speciesCounts[xi*DOUBLES_PER_AVX]);
+        avxd xh = _mm256_pow_pd(x, _mm256_set1_pd(h));
+        avxd k = _mm256_add_pd(_mm256_set1_pd(k0),_mm256_div_pd(_mm256_mul_pd(_mm256_set1_pd(dk),xh),_mm256_add_pd(_mm256_set1_pd(x0h),xh)));
+        return _mm256_mul_pd(k, _mm256_mul_pd(_mm256_load_pd(&speciesCounts[s0i*DOUBLES_PER_AVX]),_mm256_load_pd(&speciesCounts[s1i*DOUBLES_PER_AVX])));
+    }
+#endif
+
+    static PropensityFunction* create(const uint reactionIndex, const ndarray<int> S, const ndarray<uint> D, const tuple<double>k)
+    {
+        // Find the species dependencies.
+        utuple d1 = getSpecificDependencies(reactionIndex, D, 1);
+        if (d1.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one first species dependency, had",d1.len);
+        utuple d2 = getSpecificDependencies(reactionIndex, D, 2);
+        if (d2.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one second species dependency, had",d2.len);
+        utuple d3 = getSpecificDependencies(reactionIndex, D, 3);
+        if (d3.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one third species dependency, had",d3.len);
+
+        // Find the rate costant.
+        if (k.len != 4)  throw InvalidArgException("k", "second order kinetic Hill propensity needs four parameters, had",k.len);
+
+        return new SecondOrderKHillPropensity(d1[0],d2[0],d3[0],k[0],k[1],k[2],k[3]);
+    }
+
+    static lm::me::PropensityFunctionDefinition registerFunction()
+    {
+        const char* expressions[] = {"x1 * x2 * (k2 + (k3 - k2) * x3^k4 / (k1^k4 + x3^k4))", NULL};
+        const char* unitsForConstants[] = {"item", "1/(item*second)", "1/(item*second)", "1", NULL};
+        return lm::me::PropensityFunctionDefinition(REACTION_TYPE, "SecondOrderKHillPropensity", expressions, unitsForConstants, &create);
+    }
+
+    static PropensityFunction* createAlternateFormat(const uint reactionIndex, const ndarray<int> S, const ndarray<uint> D, const tuple<double>k)
+    {
+        // Find the species dependencies.
+        utuple d1 = getSpecificDependencies(reactionIndex, D, 1);
+        if (d1.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one first species dependency, had",d1.len);
+        utuple d2 = getSpecificDependencies(reactionIndex, D, 2);
+        if (d2.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one second species dependency, had",d2.len);
+        utuple d3 = getSpecificDependencies(reactionIndex, D, 3);
+        if (d3.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one third species dependency, had",d3.len);
+
+        // Find the rate costant.
+        if (k.len != 4)  throw InvalidArgException("k", "second order kinetic Hill propensity needs four parameters, had",k.len);
+
+        return new SecondOrderKHillPropensity(d1[0],d2[0],d3[0],k[0],k[1],k[1]+k[2],k[3]);
+    }
+
+    static lm::me::PropensityFunctionDefinition registerFunctionAlternateFormat()
+    {
+        const char* expressions[] = {"x1 * x2 * (k2 + k3 * x3^k4 / (k1^k4 + x3^k4))", "x1 * x2 * (k2 + k3 * (x3^k4 / (k1^k4 + x3^k4)))", NULL};
+        const char* unitsForConstants[] = {"item", "1/(item*second)", "1/(item*second)", "1", NULL};
+        return lm::me::PropensityFunctionDefinition(REACTION_TYPE_ALTERNATE_FORMAT, "SecondOrderKHillPropensity", expressions, unitsForConstants, &createAlternateFormat);
+    }
+};
+
+class SecondOrderNegativeKHillPropensity : public lm::me::PropensityFunction
+{
+public:
+    static const uint REACTION_TYPE = 8016;
+    static const uint REACTION_TYPE_ALTERNATE_FORMAT = 8017;
+
+    SecondOrderNegativeKHillPropensity(uint s0i, uint s1i, uint xi, double x0, double k0, double k1, double h) :PropensityFunction(REACTION_TYPE,1),s0i(s0i),s1i(s1i),xi(xi),x0h(pow(x0,h)),k0(k0),dk(k1-k0),h(h) {}
+    int s0i, s1i;
+    uint xi;
+    double x0h;
+    double k0;
+    double dk;
+    double h;
+
+    void changeVolume(double volumeMultiplier) {}
+    double calculate(const double time, const int* speciesCounts, const uint numberSpecies) const
+    {
+        double x = double(speciesCounts[xi]);
+        double xh = pow(x,h);
+        double k = k0+((dk*x0h)/(x0h+xh));
+        return k * double(speciesCounts[s0i]) * double(speciesCounts[s1i]);
+    }
+
+#if defined(OPT_AVX) && !defined(OPT_SVML)
+    avxd calculateAvx(const avxd time, const double* speciesCounts, const uint numberSpecies) const
+    {
+        double x0 = speciesCounts[xi*DOUBLES_PER_AVX];
+        double x1 = speciesCounts[xi*DOUBLES_PER_AVX+1];
+        double x2 = speciesCounts[xi*DOUBLES_PER_AVX+2];
+        double x3 = speciesCounts[xi*DOUBLES_PER_AVX+3];
+        avxd xh = _mm256_set_pd(pow(x3,h),pow(x2,h),pow(x1,h),pow(x0,h));
+        avxd k = _mm256_add_pd(_mm256_set1_pd(k0),_mm256_div_pd(_mm256_mul_pd(_mm256_set1_pd(dk),_mm256_set1_pd(x0h)),_mm256_add_pd(_mm256_set1_pd(x0h),xh)));
+        return _mm256_mul_pd(k, _mm256_mul_pd(_mm256_load_pd(&speciesCounts[s0i*DOUBLES_PER_AVX]),_mm256_load_pd(&speciesCounts[s1i*DOUBLES_PER_AVX])));
+    }
+#endif
+#if defined(OPT_AVX) && defined(OPT_SVML)
+    avxd calculateAvx(const avxd time, const double* speciesCounts, const uint numberSpecies) const
+    {
+        avxd x = _mm256_load_pd(&speciesCounts[xi*DOUBLES_PER_AVX]);
+        avxd xh = _mm256_pow_pd(x, _mm256_set1_pd(h));
+        avxd k = _mm256_add_pd(_mm256_set1_pd(k0),_mm256_div_pd(_mm256_mul_pd(_mm256_set1_pd(dk),_mm256_set1_pd(x0h)),_mm256_add_pd(_mm256_set1_pd(x0h),xh)));
+        return _mm256_mul_pd(k, _mm256_mul_pd(_mm256_load_pd(&speciesCounts[s0i*DOUBLES_PER_AVX]),_mm256_load_pd(&speciesCounts[s1i*DOUBLES_PER_AVX])));
+    }
+#endif
+
+    static PropensityFunction* create(const uint reactionIndex, const ndarray<int> S, const ndarray<uint> D, const tuple<double>k)
+    {
+        // Find the species dependencies.
+        utuple d1 = getSpecificDependencies(reactionIndex, D, 1);
+        if (d1.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one first species dependency, had",d1.len);
+        utuple d2 = getSpecificDependencies(reactionIndex, D, 2);
+        if (d2.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one second species dependency, had",d2.len);
+        utuple d3 = getSpecificDependencies(reactionIndex, D, 3);
+        if (d3.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one third species dependency, had",d3.len);
+
+        // Find the rate costant.
+        if (k.len != 4)  throw InvalidArgException("k", "second order kinetic Hill propensity needs four parameters, had",k.len);
+
+        return new SecondOrderNegativeKHillPropensity(d1[0],d2[0],d3[0],k[0],k[1],k[2],k[3]);
+    }
+
+    static lm::me::PropensityFunctionDefinition registerFunction()
+    {
+        const char* expressions[] = {"x1 * x2 * (k2 + (k3 - k2) * k1^k4 / (k1^k4 + x3^k4))", NULL};
+        const char* unitsForConstants[] = {"item", "1/(item*second)", "1/(item*second)", "1", NULL};
+        return lm::me::PropensityFunctionDefinition(REACTION_TYPE, "SecondOrderNegativeKHillPropensity", expressions, unitsForConstants, &create);
+    }
+
+    static PropensityFunction* createAlternateFormat(const uint reactionIndex, const ndarray<int> S, const ndarray<uint> D, const tuple<double>k)
+    {
+        // Find the species dependencies.
+        utuple d1 = getSpecificDependencies(reactionIndex, D, 1);
+        if (d1.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one first species dependency, had",d1.len);
+        utuple d2 = getSpecificDependencies(reactionIndex, D, 2);
+        if (d2.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one second species dependency, had",d2.len);
+        utuple d3 = getSpecificDependencies(reactionIndex, D, 3);
+        if (d3.len != 1) throw InvalidArgException("D", "second order kinetic Hill propensity needs one third species dependency, had",d3.len);
+
+        // Find the rate costant.
+        if (k.len != 4)  throw InvalidArgException("k", "second order kinetic Hill propensity needs four parameters, had",k.len);
+
+        return new SecondOrderNegativeKHillPropensity(d1[0],d2[0],d3[0],k[0],k[1],k[1]+k[2],k[3]);
+    }
+
+    static lm::me::PropensityFunctionDefinition registerFunctionAlternateFormat()
+    {
+        const char* expressions[] = {"x1 * x2 * (k2 + k3 * k1^k4 / (k1^k4 + x3^k4))", "x1 * x2 * (k2 + k3 * (k1^k4 / (k1^k4 + x3^k4)))", NULL};
+        const char* unitsForConstants[] = {"item", "1/(item*second)", "1/(item*second)", "1", NULL};
+        return lm::me::PropensityFunctionDefinition(REACTION_TYPE_ALTERNATE_FORMAT, "SecondOrderNegativeKHillPropensity", expressions, unitsForConstants, &createAlternateFormat);
+    }
+};
+
 list<lm::me::PropensityFunctionDefinition> GeneticCircuitPropensityFunctions::getPropensityFunctionDefinitions()
 {
     list<lm::me::PropensityFunctionDefinition> defs;
@@ -685,6 +869,10 @@ list<lm::me::PropensityFunctionDefinition> GeneticCircuitPropensityFunctions::ge
     defs.push_back(FirstOrderKHillPropensity::registerFunctionAlternateFormat());
     defs.push_back(FirstOrderNegativeKHillPropensity::registerFunction());
     defs.push_back(FirstOrderNegativeKHillPropensity::registerFunctionAlternateFormat());
+    defs.push_back(SecondOrderKHillPropensity::registerFunction());
+    defs.push_back(SecondOrderKHillPropensity::registerFunctionAlternateFormat());
+    defs.push_back(SecondOrderNegativeKHillPropensity::registerFunction());
+    defs.push_back(SecondOrderNegativeKHillPropensity::registerFunctionAlternateFormat());
     return defs;
 }
 
--- utils/c/lm_setdm.cpp
+++ utils/c/lm_setdm.cpp
@@ -1,37 +1,42 @@
 /*
  * University of Illinois Open Source License
- * Copyright 2011 Luthey-Schulten Group,
+ * Copyright 2008-2012 Luthey-Schulten Group,
+ * Copyright 2012-2017 Roberts Group,
  * All rights reserved.
- * 
+ *
  * Developed by: Luthey-Schulten Group
  * 			     University of Illinois at Urbana-Champaign
  * 			     http://www.scs.uiuc.edu/~schulten
- * 
+ *
+ * Developed by: Roberts Group
+ * 			     Johns Hopkins University
+ * 			     http://biophysics.jhu.edu/roberts/
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the Software), to deal with 
- * the Software without restriction, including without limitation the rights to 
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
- * of the Software, and to permit persons to whom the Software is furnished to 
+ * this software and associated documentation files (the Software), to deal with
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to
  * do so, subject to the following conditions:
- * 
- * - Redistributions of source code must retain the above copyright notice, 
+ *
+ * - Redistributions of source code must retain the above copyright notice,
  * this list of conditions and the following disclaimers.
- * 
- * - Redistributions in binary form must reproduce the above copyright notice, 
- * this list of conditions and the following disclaimers in the documentation 
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimers in the documentation
  * and/or other materials provided with the distribution.
- * 
+ *
  * - Neither the names of the Luthey-Schulten Group, University of Illinois at
- * Urbana-Champaign, nor the names of its contributors may be used to endorse or
- * promote products derived from this Software without specific prior written
- * permission.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL 
- * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * Urbana-Champaign, the Roberts Group, Johns Hopkins University, nor the names
+ * of its contributors may be used to endorse or promote products derived from
+ * this Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS WITH THE SOFTWARE.
  *
  * Author(s): Elijah Roberts
@@ -262,9 +267,6 @@ int main(int argc, char** argv)
                 }
 		    }
 
-		    // Set the model.
-		    file.setDiffusionModel(&model);
-
 		    // Create the lattice and lattice sites matrices.
             uint8_t * data =  new uint8_t[model.initial_lattice().lattice_x_size()*model.initial_lattice().lattice_y_size()*model.initial_lattice().lattice_z_size()*model.initial_lattice().particles_per_site()];
             for (int i=0; i<model.initial_lattice().lattice_x_size()*model.initial_lattice().lattice_y_size()*model.initial_lattice().lattice_z_size()*model.initial_lattice().particles_per_site(); i++)
@@ -316,7 +318,7 @@ int main(int argc, char** argv)
 		    }
 
 		    // Write the lattice to the file.
-            file.setDiffusionModel(&model, data, sitesData);
+            file.setDiffusionModel(&model);
 		    delete[] data;
 		    delete[] sitesData;
 
